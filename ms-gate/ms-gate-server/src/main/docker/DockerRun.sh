1b5b5db609546d0d0b7abed6c557f4d1d2050d8c
itliusir <liugang@ysstech.com>
2017-11-22 21:46:26 +0800

    edit dockerfile

--- ms-gate/ms-gate-server/src/main/docker/DockerRun.sh
+++ ms-gate/ms-gate-server/src/main/docker/DockerRun.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --restart=always -d -p 8762:8762 cfmicserver1.node/mic-service/ms-admin:1.0-SNAPSHOT  >/dev/null 2>&1
+docker run --restart=always -d -p 8762:8762 cfmicserver1.node/platformgroup/ms-gate-server:1.0-SNAPSHOT  >/dev/null 2>&1
